6cf28a0570463e638edd1710937978d2922a3301
Jamison Rubino <jamisonrubino>
2018-01-10 12:57:49 -0800

    Section 2, lecture 23

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './search_bar.js'
+import VideoList from './video_list.js'
+
+const API_KEY = 'AIzaSyAqCUuT9R2Ccnjbl7zSa17GJt3lM6PS8ak'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: []
+    }
+  }
+
+  componentDidMount() {
+    YTSearch({ key: API_KEY, term: 'term' }, videos => {
+      this.setState({videos})
+    })
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+        <VideoList
+          videos={this.state.videos}
+          />
+      </div>
     );
   }
 }
